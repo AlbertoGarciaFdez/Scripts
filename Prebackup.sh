c9174983dfc9b5db0a51a333e5522f00121201c6
Alberto Garcia <alberto.garcia.fernandez.it@gmail.com>
2014-07-15 10:35:42 +0200

    Prebackup.sh
    Script for OpenNebula Backups with Bacula

---
+++ Prebackup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+machine=$1
+backup=${machine}_Backup
+
+  if [ $(/usr/bin/onevm show ${machine} | grep LCM_STATE | cut -d ":" -f 2) == "RUNNING" ]; then
+
+    #Invoke backup tool by opennebula
+
+
+    /usr/bin/onevm disk-snapshot --live ${machine} 0 ${backup} >> /dev/null
+    #Wait for the backup to finish
+    until [  $(/usr/bin/oneimage show ${backup} | grep STATE | cut -d ":" -f 2) == "rdy" ]; do
+      echo "Not Ready" >> /dev/null
+    done
+    #OUtput the path for snapshot
+    echo $(/usr/bin/oneimage show ${backup} | grep SOURCE | cut -d ":" -f 2) 
+
+  fi
