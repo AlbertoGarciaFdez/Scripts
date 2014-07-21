a6dd5fe9fda092aeb1c0d5df2fbe033ea325e40a
Alberto Garcia <alberto.garcia.fernandez.it@gmail.com>
2014-07-21 09:08:59 +0200

    Update Prebackup.sh

--- Prebackup.sh
+++ Prebackup.sh
@@ -6,8 +6,6 @@ backup=${machine}_Backup
   if [ $(/usr/bin/onevm show ${machine} | grep LCM_STATE | cut -d ":" -f 2) == "RUNNING" ]; then
 
     #Invoke backup tool by opennebula
-
-
     /usr/bin/onevm disk-snapshot --live ${machine} 0 ${backup} >> /dev/null
     #Wait for the backup to finish
     until [  $(/usr/bin/oneimage show ${backup} | grep STATE | cut -d ":" -f 2) == "rdy" ]; do
