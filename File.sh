4c75e98ba6e41800b7b0f4196d09e3b276d68ce4
Alberto Garcia <alberto.garcia.fernandez.it@gmail.com>
2015-10-20 10:21:41 +0200

    Update and rename Prebackup.sh to File.sh

--- File.sh
+++ File.sh
@@ -2,11 +2,12 @@
 
 machine=$1
 backup=${machine}_Backup
+diskid=$(onevm show | sed -n -e '/VM DISKS/,/VM NICS/ p' | awk '/OS/ {print $1}')
 
   if [ $(/usr/bin/onevm show ${machine} | grep LCM_STATE | cut -d ":" -f 2) == "RUNNING" ]; then
 
     #Invoke backup tool by opennebula
-    /usr/bin/onevm disk-snapshot --live ${machine} 0 ${backup} >> /dev/null
+    /usr/bin/onevm disk-saveas --live ${machine} ${diskid} ${backup} >> /dev/null
     #Wait for the backup to finish
     until [  $(/usr/bin/oneimage show ${backup} | grep STATE | cut -d ":" -f 2) == "rdy" ]; do
       echo "Not Ready" >> /dev/null
